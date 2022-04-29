6fbfbd5410ff7f3303d07f8b147771c5b7d10110
nhpd <dmitry.klpv@gmail.com>
2022-04-29 11:49:39 +0300

    restructure proof files

--- internal/proofer/proofs/all_balances.go
+++ internal/proofer/proofs/all_balances.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/lidofinance/cosmos-query-relayer/internal/proofer"
 )
 
@@ -49,56 +48,6 @@ func ProofAllBalances(ctx context.Context, address string, denom string, querier
 		return nil, err
 	}
 	fmt.Printf("\nCoin: %v, Height %v Err %v", amount, height, err)
-
-	return nil, nil
-}
-
-func ProofAllDelegations(ctx context.Context, validators []string, delegator string, querier *proofer.QueryKeyProofer) (map[string]string, error) {
-	inputHeight := int64(0)
-	storeKey := stakingtypes.StoreKey
-	delegatorBz, err := cosmostypes.GetFromBech32(delegator, "terra")
-	if err != nil {
-		return nil, err
-	}
-
-	//delegatorPrefixKey := stakingtypes.GetDelegationsKey(delegatorBz)
-	for _, validator := range validators {
-		validatorBz, err := cosmostypes.GetFromBech32(validator, "terravaloper")
-		if err != nil {
-			return nil, err
-		}
-
-		key := stakingtypes.GetDelegationKey(delegatorBz, validatorBz)
-		fmt.Println("processing delegation...")
-		//key := append(banktypes.CreateAccountBalancesPrefix(bz), []byte(denom)...)
-		value, height, err := querier.QueryTendermintProof(ctx, querier.ChainID, inputHeight, storeKey, key)
-
-		var delegation stakingtypes.Delegation
-		if err := delegation.Unmarshal(value.Value); err != nil {
-			fmt.Println("err: %s", err)
-			return nil, err
-		}
-		fmt.Printf("\nDelegation:\n %v, Height %v Err %v", delegation, height, err)
-	}
-
-	return nil, nil
-}
-
-func ProofAllDelegations2(ctx context.Context, delegator string, querier *proofer.QueryKeyProofer) (map[string]string, error) {
-	inputHeight := int64(0)
-	storeKey := stakingtypes.StoreKey
-	delegatorBz, err := cosmostypes.GetFromBech32(delegator, "terra")
-	if err != nil {
-		return nil, err
-	}
-
-	delegatorPrefixKey := stakingtypes.GetDelegationsKey(delegatorBz)
-	result, height, err := querier.QueryIterateTendermintProof(ctx, querier.ChainID, inputHeight, storeKey, delegatorPrefixKey)
-	if err != nil {
-		fmt.Printf("\nerr: %s", err)
-		return nil, err
-	}
-	fmt.Printf("\nResult: %+v\nHeight: %+v", result, height)
 
 	return nil, nil
 }
---
+++ internal/proofer/proofs/all_delegator_delegations.go
@@ -0,0 +1,59 @@
+package proofs
+
+import (
+	"context"
+	"fmt"
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/lidofinance/cosmos-query-relayer/internal/proofer"
+)
+
+func ProofAllDelegations(ctx context.Context, validators []string, delegator string, querier *proofer.QueryKeyProofer) (map[string]string, error) {
+	inputHeight := int64(0)
+	storeKey := stakingtypes.StoreKey
+	delegatorBz, err := cosmostypes.GetFromBech32(delegator, "terra")
+	if err != nil {
+		return nil, err
+	}
+
+	//delegatorPrefixKey := stakingtypes.GetDelegationsKey(delegatorBz)
+	for _, validator := range validators {
+		validatorBz, err := cosmostypes.GetFromBech32(validator, "terravaloper")
+		if err != nil {
+			return nil, err
+		}
+
+		key := stakingtypes.GetDelegationKey(delegatorBz, validatorBz)
+		fmt.Println("processing delegation...")
+		//key := append(banktypes.CreateAccountBalancesPrefix(bz), []byte(denom)...)
+		value, height, err := querier.QueryTendermintProof(ctx, querier.ChainID, inputHeight, storeKey, key)
+
+		var delegation stakingtypes.Delegation
+		if err := delegation.Unmarshal(value.Value); err != nil {
+			fmt.Println("err: %s", err)
+			return nil, err
+		}
+		fmt.Printf("\nDelegation:\n %v, Height %v Err %v", delegation, height, err)
+	}
+
+	return nil, nil
+}
+
+func ProofAllDelegations2(ctx context.Context, delegator string, querier *proofer.QueryKeyProofer) (map[string]string, error) {
+	inputHeight := int64(0)
+	storeKey := stakingtypes.StoreKey
+	delegatorBz, err := cosmostypes.GetFromBech32(delegator, "terra")
+	if err != nil {
+		return nil, err
+	}
+
+	delegatorPrefixKey := stakingtypes.GetDelegationsKey(delegatorBz)
+	result, height, err := querier.QueryIterateTendermintProof(ctx, querier.ChainID, inputHeight, storeKey, delegatorPrefixKey)
+	if err != nil {
+		fmt.Printf("\nerr: %s", err)
+		return nil, err
+	}
+	fmt.Printf("\nResult: %+v\nHeight: %+v", result, height)
+
+	return nil, nil
+}
