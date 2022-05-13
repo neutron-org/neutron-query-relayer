700ec13b7dd3c9e4f8adc8548545130056fee977
nhpd <dmitry.klpv@gmail.com>
2022-05-13 20:19:01 +0300

    works

--- internal/submitter/submitter.go
+++ internal/submitter/submitter.go
@@ -10,18 +10,18 @@ import (
 	"github.com/cosmos/cosmos-sdk/types"
 	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	rpcclient "github.com/tendermint/tendermint/rpc/client"
 )
 
 //var mode = signing.SignMode_SIGN_MODE_LEGACY_AMINO_JSON
-var height int64 = 0
 
 var mode = signing.SignMode_SIGN_MODE_DIRECT
 
 type TxSubmitter struct {
 	ctx           context.Context
-	txf           tx.Factory
+	baseTxf       tx.Factory
 	codec         Codec
 	rpcClient     rpcclient.Client
 	chainID       string
@@ -34,52 +34,61 @@ func NewTxSubmitter(ctx context.Context, rpcClient rpcclient.Client, chainID str
 	if err != nil {
 		return nil, err
 	}
+	baseTxf := tx.Factory{}.
+		WithChainID(chainID).
+		WithTxConfig(codec.TxConfig).
+		WithGasAdjustment(gasAdj).
+		WithGasPrices(gasPrices).
+		WithKeybase(keybase).
+		WithSignMode(mode)
 	return &TxSubmitter{
 		ctx:           ctx,
 		codec:         codec,
-		txf:           TxFactory(chainID, codec, gasAdj, gasPrices, keybase),
+		baseTxf:       baseTxf,
 		rpcClient:     rpcClient,
 		chainID:       chainID,
 		addressPrefix: addressPrefix,
 	}, nil
 }
 
-func TxFactory(chainId string, codec Codec, gasAdjustment float64, gasPrices string, keybase keyring.Keyring) tx.Factory {
-	return tx.Factory{}.
-		//WithAccountRetriever(cc).
-		WithChainID(chainId).
-		WithTxConfig(codec.TxConfig).
-		WithGasAdjustment(gasAdjustment).
-		WithGasPrices(gasPrices).
-		WithKeybase(keybase).
-		WithSignMode(mode)
-}
-
 // TODO: submits query with proof back to lido chain
 func (cc *TxSubmitter) Send(address1, address2 string) error {
-	fmt.Printf("Trying to send from / to =: %v / %v\n", address1, address2)
+	fmt.Printf("About to Send coins from / to =: %v / %v\n", address1, address2)
+
 	msgs, err := cc.buildMsgs(address1, address2)
 	if err != nil {
 		return err
 	}
-	gasNeeded, err := cc.calculateGas(msgs...)
-	fmt.Printf("Gas needed: %+v\n", gasNeeded)
+
+	account, err := cc.QueryAccount(address1)
 	if err != nil {
 		return err
 	}
-	bz, err := cc.buildTxBz(msgs, address1, gasNeeded)
+
+	txf := cc.baseTxf.
+		WithAccountNumber(account.AccountNumber).
+		WithSequence(account.Sequence)
+
+	gasNeeded, err := cc.calculateGas(txf, msgs...)
+	if err != nil {
+		return err
+	}
+
+	txf = txf.WithGas(gasNeeded)
+
+	bz, err := cc.buildTxBz(txf, msgs, address1, gasNeeded)
 	if err != nil {
 		return err
 	}
 	res, err := cc.rpcClient.BroadcastTxSync(cc.ctx, bz)
 
-	fmt.Printf("Broadcast result: res=%+v err=%+v", res.Log, err)
+	fmt.Printf("Broadcast result: code=%+v log=%v err=%+v", res.Code, res.Log, err)
 
 	return nil
 }
 
 func (cc *TxSubmitter) buildMsgs(address1, address2 string) ([]types.Msg, error) {
-	amount := types.NewCoins(types.NewInt64Coin("uluna", 100))
+	amount := types.NewCoins(types.NewInt64Coin("uluna", 100000))
 	msg := &banktypes.MsgSend{FromAddress: address1, ToAddress: address2, Amount: amount}
 
 	err := msg.ValidateBasic()
@@ -87,26 +96,20 @@ func (cc *TxSubmitter) buildMsgs(address1, address2 string) ([]types.Msg, error)
 		return nil, err
 	}
 	signedMsg := msg.GetSignBytes()
-	fmt.Printf("\nSIGNED Tx: %+v\n\n", string(signedMsg))
+	fmt.Printf("\nBuilt Tx info: %+v\n\n", string(signedMsg))
 
 	return []types.Msg{msg}, nil
 }
 
-func (cc *TxSubmitter) buildTxBz(msgs []types.Msg, feePayerAddress string, gasAmount uint64) ([]byte, error) {
+func (cc *TxSubmitter) buildTxBz(txf tx.Factory, msgs []types.Msg, feePayerAddress string, gasAmount uint64) ([]byte, error) {
 	txBuilder := cc.codec.TxConfig.NewTxBuilder()
-	//aminoConfig := legacytx.StdTxConfig{Cdc: cc.codec.Amino}
-	//txBuilder := aminoConfig.NewTxBuilder()
 	err := txBuilder.SetMsgs(msgs...)
-	//signatures :=
-	//txBuilder.SetSignatures(signatures)
 	if err != nil {
 		fmt.Printf("set msgs failure")
 		return nil, err
 	}
 
-	cc.txf.WithGas(gasAmount)
 	txBuilder.SetGasLimit(gasAmount) // TODO: correct?
-	//txBuilder.SetFeeAmount(...)
 	txBuilder.SetMemo("bob to alice")
 
 	feePayerBz, err := types.GetFromBech32(feePayerAddress, cc.addressPrefix)
@@ -114,12 +117,13 @@ func (cc *TxSubmitter) buildTxBz(msgs []types.Msg, feePayerAddress string, gasAm
 		return nil, err
 	}
 	txBuilder.SetFeePayer(feePayerBz)
+	// TODO: correct?
 	txBuilder.SetFeeAmount(types.NewCoins(types.NewInt64Coin("uluna", 2000)))
 	//txBuilder.SetFeeGranter()
-	//txBuilder.SetFeeAmount()
 	//txBuilder.SetTimeoutHeight(...)
 
-	err = tx.Sign(cc.txf, "bob", txBuilder, true)
+	fmt.Printf("\nAbout to sign with txf: %+v\n\n", txf)
+	err = tx.Sign(txf, "bob", txBuilder, true)
 
 	if err != nil {
 		return nil, err
@@ -129,8 +133,8 @@ func (cc *TxSubmitter) buildTxBz(msgs []types.Msg, feePayerAddress string, gasAm
 	return bz, err
 }
 
-func (cc *TxSubmitter) calculateGas(msgs ...types.Msg) (uint64, error) {
-	simulation, err := cc.BuildSimTx(msgs...)
+func (cc *TxSubmitter) calculateGas(txf tx.Factory, msgs ...types.Msg) (uint64, error) {
+	simulation, err := cc.BuildSimTx(txf, msgs...)
 	if err != nil {
 		return 0, err
 	}
@@ -139,15 +143,10 @@ func (cc *TxSubmitter) calculateGas(msgs ...types.Msg) (uint64, error) {
 		Path: "/cosmos.tx.v1beta1.Service/Simulate",
 		Data: simulation,
 	}
-	opts := rpcclient.ABCIQueryOptions{
-		Height: height,
-		Prove:  false,
-	}
-	res, err := cc.rpcClient.ABCIQueryWithOptions(cc.ctx, simQuery.Path, simQuery.Data, opts)
+	res, err := cc.rpcClient.ABCIQueryWithOptions(cc.ctx, simQuery.Path, simQuery.Data, rpcclient.DefaultABCIQueryOptions)
 	if err != nil {
 		return 0, err
 	}
-	//fmt.Printf("Simulate res: %+v\n", res) // Prints estimated gas used.
 
 	var simRes txtypes.SimulateResponse
 
@@ -155,24 +154,16 @@ func (cc *TxSubmitter) calculateGas(msgs ...types.Msg) (uint64, error) {
 		return 0, err
 	}
 	if simRes.GasInfo == nil {
-		return 0, fmt.Errorf("no result in simulation response")
+		return 0, fmt.Errorf("no result in simulation response: %+v", simRes)
 	}
 
-	//fmt.Printf("Simulate Result: %+v\n", simRes) // Prints estimated gas used.
-
-	return uint64(cc.txf.GasAdjustment() * float64(simRes.GasInfo.GasUsed)), nil
-}
-
-// protoTxProvider is a type which can provide a proto transaction. It is a
-// workaround to get access to the wrapper TxBuilder's method GetProtoTx().
-type protoTxProvider interface {
-	GetProtoTx() *txtypes.Tx
+	return uint64(txf.GasAdjustment() * float64(simRes.GasInfo.GasUsed)), nil
 }
 
 // BuildSimTx creates an unsigned tx with an empty single signature and returns
 // the encoded transaction or an error if the unsigned transaction cannot be built.
-func (cc *TxSubmitter) BuildSimTx(msgs ...types.Msg) ([]byte, error) {
-	txb, err := cc.txf.BuildUnsignedTx(msgs...)
+func (cc *TxSubmitter) BuildSimTx(txf tx.Factory, msgs ...types.Msg) ([]byte, error) {
+	txb, err := cc.baseTxf.BuildUnsignedTx(msgs...)
 	if err != nil {
 		return nil, err
 	}
@@ -182,19 +173,50 @@ func (cc *TxSubmitter) BuildSimTx(msgs ...types.Msg) ([]byte, error) {
 	sig := signing.SignatureV2{
 		PubKey: &secp256k1.PubKey{},
 		Data: &signing.SingleSignatureData{
-			SignMode: cc.txf.SignMode(),
+			SignMode: cc.baseTxf.SignMode(),
 		},
-		Sequence: cc.txf.Sequence(),
+		Sequence: txf.Sequence(),
 	}
 	if err := txb.SetSignatures(sig); err != nil {
 		return nil, err
 	}
 
-	protoProvider, ok := txb.(protoTxProvider)
-	if !ok {
-		return nil, fmt.Errorf("cannot simulate amino tx")
+	bz, err := cc.codec.TxConfig.TxEncoder()(txb.GetTx())
+	if err != nil {
+		return nil, nil
+	}
+	simReq := txtypes.SimulateRequest{TxBytes: bz}
+	return simReq.Marshal()
+}
+
+// QueryAccount returns BaseAccount for given account address
+func (cc *TxSubmitter) QueryAccount(address string) (*authtypes.BaseAccount, error) {
+	request := authtypes.QueryAccountRequest{Address: address}
+	req, err := request.Marshal()
+	if err != nil {
+		return nil, err
+	}
+	// We then call the Simulate method on this client.
+	simQuery := abci.RequestQuery{
+		Path: "/cosmos.auth.v1beta1.Query/Account",
+		Data: req,
+	}
+	res, err := cc.rpcClient.ABCIQueryWithOptions(cc.ctx, simQuery.Path, simQuery.Data, rpcclient.DefaultABCIQueryOptions)
+	if err != nil {
+		return nil, err
 	}
 
-	simReq := txtypes.SimulateRequest{Tx: protoProvider.GetProtoTx()}
-	return simReq.Marshal()
+	var response authtypes.QueryAccountResponse
+	if err := response.Unmarshal(res.Response.Value); err != nil {
+		return nil, err
+	}
+
+	var account authtypes.BaseAccount
+	err = account.Unmarshal(response.Account.Value)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &account, nil
 }
