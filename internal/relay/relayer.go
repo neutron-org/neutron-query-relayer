043f984d892e48999c27819cd2d3b3fcf635b31f
pr0n00gler <programmer10110@gmail.com>
2022-06-21 14:33:47 +0300

    getHeaderWithBestTrustedHeight()

--- internal/relay/relayer.go
+++ internal/relay/relayer.go
@@ -5,13 +5,16 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	lidotypes "github.com/lidofinance/gaia-wasm-zone/x/interchainqueries/types"
+	"math"
 	"strconv"
 
 	"github.com/avast/retry-go/v4"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
 	ibcexported "github.com/cosmos/ibc-go/v3/modules/core/exported"
+	tmclient "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
 	"github.com/cosmos/relayer/v2/relayer"
 	"github.com/cosmos/relayer/v2/relayer/provider"
 	"github.com/cosmos/relayer/v2/relayer/provider/cosmos"
@@ -187,9 +190,14 @@ func (r Relayer) proofMessage(ctx context.Context, m queryEventMessage) error {
 			return fmt.Errorf("could not get proof for RecipientTransactions with query_id=%d: %w", m.queryId, err)
 		}
 
+		consensusStates, err := r.getConsensusStates(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to get consensus states: %w", err)
+		}
+
 		resultBlocks := make([]*lidotypes.Block, 0, len(blocks))
 		for height, txs := range blocks {
-			header, err := r.getSrcChainHeader(ctx, int64(height))
+			header, err := r.getHeaderWithBestTrustedHeight(ctx, consensusStates, height)
 			if err != nil {
 				return fmt.Errorf("failed to get header for src chain: %w", err)
 			}
@@ -199,7 +207,7 @@ func (r Relayer) proofMessage(ctx context.Context, m queryEventMessage) error {
 				return fmt.Errorf("failed to pack header: %w", err)
 			}
 
-			nextHeader, err := r.getSrcChainHeader(ctx, int64(height+1))
+			nextHeader, err := r.getHeaderWithBestTrustedHeight(ctx, consensusStates, height+1)
 			if err != nil {
 				return fmt.Errorf("failed to get next header for src chain: %w", err)
 			}
@@ -229,6 +237,81 @@ func (r Relayer) proofMessage(ctx context.Context, m queryEventMessage) error {
 	}
 
 	return nil
+}
+
+// getConsensusStates returns light client consensus states from lido chain
+func (r *Relayer) getConsensusStates(ctx context.Context) ([]clienttypes.ConsensusStateWithHeight, error) {
+	// Without this hack it doesn't want to work with NewQueryClient
+	provConcreteLidoChain, ok := r.lidoChain.ChainProvider.(*cosmos.CosmosProvider)
+	if !ok {
+		return nil, fmt.Errorf("failed to cast ChainProvider to concrete type (cosmos.CosmosProvider)")
+	}
+
+	qc := clienttypes.NewQueryClient(provConcreteLidoChain)
+
+	consensusStatesResponse, err := qc.ConsensusStates(ctx, &clienttypes.QueryConsensusStatesRequest{
+		ClientId: r.lidoChain.ClientID(),
+		Pagination: &query.PageRequest{
+			// TODO: paging
+			Limit:      math.MaxUint64,
+			Reverse:    true,
+			CountTotal: true,
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get consensus states for client ID %s: %w", r.lidoChain.ClientID(), err)
+	}
+
+	return consensusStatesResponse.ConsensusStates, nil
+}
+
+// getHeaderWithBestTrustedHeight returns an IBC Update Header which can be used to update an on chain
+// light client on the Lido chain.
+//
+// It has the same purpose as r.targetChain.ChainProvider.GetIBCUpdateHeader() but the difference is
+// that getHeaderWithBestTrustedHeight() trys to find the best TrustedHeight for the header
+// relying on existing light client's consensus states on the Lido chain.
+
+// The best trusted height for the height in this case is the closest one to some existed consensus state's height but not less
+func (r *Relayer) getHeaderWithBestTrustedHeight(ctx context.Context, consensusStates []clienttypes.ConsensusStateWithHeight, height uint64) (ibcexported.Header, error) {
+	minDiff := uint64(0)
+	bestTrustedHeight := clienttypes.Height{
+		RevisionNumber: 0,
+		RevisionHeight: 0,
+	}
+
+	for _, cs := range consensusStates {
+		if bestTrustedHeight.IsZero() {
+			bestTrustedHeight = cs.GetHeight()
+			minDiff = height - cs.Height.RevisionHeight
+			continue
+		}
+
+		if height >= cs.Height.RevisionHeight && (height-cs.Height.RevisionHeight) < minDiff {
+			bestTrustedHeight = cs.Height
+			minDiff = height - cs.Height.RevisionHeight
+		}
+	}
+
+	// Without this hack we can't call InjectTrustedFields
+	provConcreteTargetChain, ok := r.targetChain.ChainProvider.(*cosmos.CosmosProvider)
+	if !ok {
+		return nil, fmt.Errorf("failed to cast ChainProvider to concrete type (cosmos.CosmosProvider)")
+	}
+
+	header, err := r.targetChain.ChainProvider.GetLightSignedHeaderAtHeight(ctx, int64(height))
+	if err != nil {
+		return nil, err
+	}
+
+	tmHeader, ok := header.(*tmclient.Header)
+	if !ok {
+		return nil, fmt.Errorf("trying to inject fields into non-tendermint headers")
+	}
+
+	tmHeader.TrustedHeight = bestTrustedHeight
+
+	return provConcreteTargetChain.InjectTrustedFields(ctx, tmHeader, r.lidoChain.ChainProvider, r.lidoChain.PathEnd.ClientID)
 }
 
 func (r *Relayer) getSrcChainHeader(ctx context.Context, height int64) (ibcexported.Header, error) {
