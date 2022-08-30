ce0b6b7ecfe9ff96418f17f9c1fee3f85985179a
sotnikov-s <ssotnikov93@gmail.com>
2022-08-30 11:05:30 +0300

    make timestamps readable for zap logger

--- internal/config/logger_confg.go
+++ internal/config/logger_confg.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/kelseyhightower/envconfig"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 const loggerPrefix = "LOGGER"
@@ -10,6 +11,7 @@ const loggerPrefix = "LOGGER"
 // NewLoggerConfig  initializes a default production config w parameters, overwritten by env vars if present
 func NewLoggerConfig() (*zap.Config, error) {
 	cfg := zap.NewProductionConfig()
+	cfg.EncoderConfig.EncodeTime = zapcore.RFC3339NanoTimeEncoder
 	err := envconfig.Process(loggerPrefix, &cfg)
 	if err != nil {
 		return nil, err
