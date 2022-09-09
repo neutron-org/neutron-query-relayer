bd3d7d72eac845210687e305e4a1e2e47ce162c4
nhpd <dmitry.klpv@gmail.com>
2022-09-09 16:24:02 +0300

    post-review fixes: fix comment

--- internal/relay/trusted_headers.go
+++ internal/relay/trusted_headers.go
@@ -8,6 +8,6 @@ import (
 
 // TrustedHeaderFetcher able to get trusted headers for a given height
 type TrustedHeaderFetcher interface {
-	// Fetch returns two Headers for height and height+1 packed into *codectypes.Any value for
+	// Fetch returns two trusted Headers for height and height+1 packed into *codectypes.Any value
 	Fetch(ctx context.Context, height uint64) (header *codectypes.Any, nextHeader *codectypes.Any, err error)
 }
