23ae245002a7affe1abf2c7f7607ff23933014a3
Murad Karammaev <muradkarammaev@gmail.com>
2023-03-29 13:50:16 +0300

    fix: local run

--- internal/subscriber/querier/client/query/utils.go
+++ internal/subscriber/querier/client/query/utils.go
@@ -24,16 +24,18 @@ func (o *NeutronInterchainQueriesRegisteredQueriesOKBodyRegisteredQueriesItems0)
 		return nil, fmt.Errorf("failed to parse o.LastSubmittedResultLocalHeight: %w", err)
 	}
 
-	lastSubmittedResultRemoteRevisionNumber, err := strconv.ParseUint(o.LastSubmittedResultRemoteHeight.RevisionNumber, 10, 64)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse o.LastSubmittedResultLocalHeight: %w", err)
-	}
+	lastSubmittedResultRemoteRevisionNumber, lastSubmittedResultRemoteRevisionHeight := uint64(0), uint64(0)
+	if o.LastSubmittedResultRemoteHeight != nil {
+		lastSubmittedResultRemoteRevisionNumber, err = strconv.ParseUint(o.LastSubmittedResultRemoteHeight.RevisionNumber, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse o.LastSubmittedResultLocalHeight: %w", err)
+		}
 
-	lastSubmittedResultRemoteRevisionHeight, err := strconv.ParseUint(o.LastSubmittedResultRemoteHeight.RevisionHeight, 10, 64)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse o.LastSubmittedResultLocalHeight: %w", err)
+		lastSubmittedResultRemoteRevisionHeight, err = strconv.ParseUint(o.LastSubmittedResultRemoteHeight.RevisionHeight, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse o.LastSubmittedResultLocalHeight: %w", err)
+		}
 	}
-
 	queryHeight := ibcclienttypes.NewHeight(lastSubmittedResultRemoteRevisionNumber, lastSubmittedResultRemoteRevisionHeight)
 
 	var keys []*neutrontypes.KVKey
