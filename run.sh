9e3423a035af09dc3e739034bed058712dfafbf1
Murad <muradkarammaev@gmail.com>
2025-05-20 20:44:09 +0300

    fix: increase wait condition in run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 NODE=${NODE:-node}
-echo "Waiting for a first block..."
-while ! curl -f ${NODE}:1317/cosmos/base/tendermint/v1beta1/blocks/1 >/dev/null 2>&1; do
+echo "Waiting for first 10 blocks on $NODE..."
+while ! curl -f ${NODE}:1317/cosmos/base/tendermint/v1beta1/blocks/10 ; do
+  echo
   sleep 1
 done
+echo
 echo "Start relayer"
 neutron_query_relayer start
